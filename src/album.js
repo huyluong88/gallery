bef70722c63c8ae0645d39ac4eeb3d729ed36364
Huy Luong <huyluong@huys-mbp.attlocal.net>
2017-01-21 14:56:24 -0500

    updated files for portfolio

--- src/album.js
+++ src/album.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react'
 import cars from './data'
+import { Link } from 'react-router'
+
 
 class Album extends Component{
  render(){
@@ -7,10 +9,11 @@ class Album extends Component{
    var album = cars[index]
    return (
      <div>
+        <Link to="/">Back to main page</Link>
         <h1>{album.make}</h1>
         <ul>
         {album.photos.map(photo => {
-          return (<li><img src={photo.url}/>
+          return (<li><img className="album" src={photo.url}/>
               <p>{photo.desc}</p>
           </li>)
         })}
